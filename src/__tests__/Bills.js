e397f1ec5de6140054b112be00cdce29af0152c9
leo23225 <leo_94310@hotmail.com>
2023-11-22 19:15:48 +0100

    bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -29,10 +29,20 @@ describe("Given I am connected as an employee", () => {
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
+      // Contenu HTML du document -> modifié
+      // -> injection du contenu généré par la fonction BillsUI -> utilisée pour afficher les notes de frais.
       document.body.innerHTML = BillsUI({ data: bills })
+      // dates = tableau dates contenant tous les éléments spécifié
+      // utilisation de Regex
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      // antiChrono = Fonction de tri des dates de la plus récente à la plus ancienne.
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      // datesSorted stocke ce trie du tableau dates en utilisant la fonction de comparaison antiChrono 
+      // -> le résultat trié est stocké dans le nouveau tableau.
       const datesSorted = [...dates].sort(antiChrono)
+      // Si les deux tableaux sont égaux -> test success
+      // (signifiant que les dates extraites de l'affichage sont triées de la plus récente à la plus ancienne
+      // comme spécifié dans la description du test).
       expect(dates).toEqual(datesSorted)
     })
   })
