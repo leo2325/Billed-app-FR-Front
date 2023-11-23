625241dec1350c97ce45461c8278e5eb26f04068
leo23225 <leo_94310@hotmail.com>
2023-11-23 13:50:56 +0100

    erase comments

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -20,30 +20,6 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  // vérifie si data existe.
-  // Si data true et/donc sa longueur > 0, la condition est évaluée comme vraie (true)
-  
-  // Opération de mapping (map): 
-  // La méthode map() crée un nouveau tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément du tableau appelant.
-  // Si la condition du ternaire est vraie, 
-  // la fonction map est utilisée sur le tableau des données. 
-  // Cela signifie que pour chaque élément du tableau data, la fonction fléchée (bill => row(bill)) est appliquée. 
-  // Cette fonction prend un élément bill du tableau et appelle une fonction appelée row avec bill comme argument.
-
-  // Méthode join("") est ensuite appelée sur le résultat du map. 
-  // Cela combine tous les éléments du tableau résultant en une seule chaîne de caractères. 
-  // L'argument de la méthode join ("" dans ce cas) spécifie le séparateur entre les éléments dans la chaîne résultante. 
-  // En utilisant "", les éléments seront concaténés sans aucun espace entre eux.
-
-  // Valeur de retour: 
-  // La valeur de retour de la fonction rows est la chaîne résultante après le join. 
-  // Enfin Si la condition du ternaire était fausse (c'est-à-dire si data n'existait pas ou était un tableau vide), 
-  // la fonction renvoie une chaîne vide ("").
-  
-
-  // En résumé, cette fonction prend un tableau de données (data), 
-  // applique une fonction row à chaque élément de ce tableau, 
-  // puis concatène les résultats en une seule chaîne de caractères.s
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
